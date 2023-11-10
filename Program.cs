07302efb97f310606af8810fcc7591a1d4d47cc3
Mark Kevin Pulga <mvpulga@lbcexpress.com>
2023-11-10 15:22:07 +0800

    sample new branch

--- Program.cs
+++ Program.cs
@@ -7,23 +7,6 @@ namespace My_Awesome_Project
     {
         static void Main(string[] args)
         {
-            Console.Title="SkyNet";
-        
-            Console.WriteLine("A proud knight named...");
-            string name = Console.ReadLine();
-            
-            Console.WriteLine($"{name} Walked into a bar, At the counter he met a ...");
-            string friendName = Console.ReadLine();
-
-            Console.WriteLine($"{friendName} asked him what he wanted to drink?");
-            string drink = Console.ReadLine();
-
-            Console.WriteLine($"{drink}! shouted {name} {friendName} shushed {name} quickly.");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"But it was too late... A dragon ate them {name} and {friendName}.");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("---The End----");
-            
             
             Console.ReadKey();
         }
