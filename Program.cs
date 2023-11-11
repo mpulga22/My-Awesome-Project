85b0926129ccb6787684636b1ab3e95c68e1408e
Mark Kevin Pulga <mvpulga@lbcexpress.com>
2023-11-11 15:39:23 +0800

    2diceroll

--- Program.cs
+++ Program.cs
@@ -8,63 +8,36 @@ namespace My_Awesome_Project
     {
         static void Main(string[] args)
         {
-            string[] equations = new string[3];
-            int[] answers = new int[3];
-            for(int a = 0; a <= equations.Length-1;a++){
-
-                #region form equation
-            Random random = new Random();
-            int[] numbers = new int[3];
-
-            for(int number=0;number <= numbers.Length-1;number++){
-                numbers[number] =   random.Next(1,9);
-            }
-
-            string[] operations = {"+","-","*","/"};
-            string[] usedoperations = new string[2];
-            
-            for(int i = 0; i <= usedoperations.Length - 1 ; i++){
-                usedoperations[i] = operations[random.Next(0,operations.Length-1)];
-            }
-          
-            for(int i = 0; i<=numbers.Length-1;i++){
-
-                if(i>0){
-                   equations[a]+= usedoperations[i-1];
-
-                   switch(usedoperations[i-1]){
-                    case "+":
-                        answers[a]+=numbers[i];
-                        break;
-                    case "-":
-                        answers[a]-=numbers[i];
-                        break;
-                    case "*":
-                        answers[a]*=numbers[i];
-                        break;
-                    case "/":
-                        answers[a]/=numbers[i];
-                        break;
-                    
-                   }
-                }else{
-                     answers[a]+=numbers[i];
-                }
-                     equations[a]+= (numbers[i]);
-            }
-
-           #endregion
-               Console.Write(equations[a]+"=");   
-                int input = Convert.ToInt32(Console.ReadLine());
-
-            if(input == answers[a]){
-                Console.WriteLine("correct");
-            }else{
-                 Console.WriteLine("Incorrect");
-             
-            }  
-                      }
+            Console.WriteLine("Press enter to roll the dice");
+         //   int count = Convert.ToInt32(Console.ReadLine());
            
+        //    for (int i = 1; i <= count; i++)
+        //    {    
+        //         int result = Convert.ToInt32(Math.Pow(2,i));
+        //         //raise 2^
+        //         Console.WriteLine(result);
+        //    }
+            
+            Random random = new Random();
+
+            int roll1 = 0;
+            int roll2 = 1;
+
+            int attempts = 0;
+               
+            while(roll1!=roll2){
+                Console.ReadKey();
+                attempts+=1;
+                roll1 = random.Next(1,7); 
+                roll2 = random.Next(1,7);                 
+                Console.WriteLine($"attempt:{attempts}");
+                Console.WriteLine($"Dice 1:{roll1}, Dice 2 : {roll2}");
+                
+            }
+            
+                Console.WriteLine($"It took you {attempts} attemps to have {roll1} and {roll2} together");
+              
+
              //wait before closing
             Console.ReadKey();
         }
