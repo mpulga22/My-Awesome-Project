5061d7418cce979dd9681580450580b3792aedbf
Mark Kevin Pulga <mvpulga@lbcexpress.com>
2023-11-11 14:52:56 +0800

    simple math game

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Xml;
 
 namespace My_Awesome_Project
@@ -7,7 +8,64 @@ namespace My_Awesome_Project
     {
         static void Main(string[] args)
         {
+            string[] equations = new string[3];
+            int[] answers = new int[3];
+            for(int a = 0; a <= equations.Length-1;a++){
+
+                #region form equation
+            Random random = new Random();
+            int[] numbers = new int[3];
+
+            for(int number=0;number <= numbers.Length-1;number++){
+                numbers[number] =   random.Next(1,9);
+            }
+
+            string[] operations = {"+","-","*","/"};
+            string[] usedoperations = new string[2];
             
+            for(int i = 0; i <= usedoperations.Length - 1 ; i++){
+                usedoperations[i] = operations[random.Next(0,operations.Length-1)];
+            }
+          
+            for(int i = 0; i<=numbers.Length-1;i++){
+
+                if(i>0){
+                   equations[a]+= usedoperations[i-1];
+
+                   switch(usedoperations[i-1]){
+                    case "+":
+                        answers[a]+=numbers[i];
+                        break;
+                    case "-":
+                        answers[a]-=numbers[i];
+                        break;
+                    case "*":
+                        answers[a]*=numbers[i];
+                        break;
+                    case "/":
+                        answers[a]/=numbers[i];
+                        break;
+                    
+                   }
+                }else{
+                     answers[a]+=numbers[i];
+                }
+                     equations[a]+= (numbers[i]);
+            }
+
+           #endregion
+               Console.Write(equations[a]+"=");   
+                int input = Convert.ToInt32(Console.ReadLine());
+
+            if(input == answers[a]){
+                Console.WriteLine("correct");
+            }else{
+                 Console.WriteLine("Incorrect");
+             
+            }  
+                      }
+           
+             //wait before closing
             Console.ReadKey();
         }
     }
